02c8fcd034bc5132cc6cc28ddcb01dedbb367429
alishbamusharraf <162012518+alishbamusharraf@users.noreply.github.com>
2024-03-09 23:18:20 +0500

    assignment done

---
+++ main.js
@@ -0,0 +1,26 @@
+// class 1
+console.log("helloworld");
+// class 2
+//DATA TYPE 
+var myname = 'alishba';
+console.log(myname);
+var age = 17;
+console.log(age);
+var ispass = true;
+console.log(ispass);
+var abcd = null;
+console.log(abcd);
+var xyz;
+console.log(xyz);
+var userName = "alishba"; // e.g.(camel case)
+var user_name = "amal"; // e.g.(snake case)
+var UserName = "hamza"; // e.g.(pascal case)
+var isuserExist = true;
+var is_user_exist = false;
+var IsUserExist = 1;
+console.log(userName, user_name, UserName);
+console.log(isuserExist, is_user_exist, IsUserExist);
+// ARRAY []
+var students = ['laiba,maham,kainat,uzma'];
+var student = [4];
+console.log(students);
---
+++ main.ts
@@ -0,0 +1,38 @@
+// class 1
+
+console.log("helloworld")
+
+// class 2
+//DATA TYPE 
+ 
+ let myname: string= 'alishba';
+ console.log(myname);
+
+ let age :number=17;
+ console.log(age);
+
+let ispass:boolean=true;
+console.log(ispass);
+
+let abcd:null=null
+console.log(abcd)
+
+let xyz:undefined
+console.log(xyz)
+
+let userName="alishba"// e.g.(camel case)
+let user_name="amal"// e.g.(snake case)
+let UserName="hamza"// e.g.(pascal case)
+
+let isuserExist= true;
+let is_user_exist=false;
+let IsUserExist=1;
+
+console.log(userName,user_name,UserName)
+console.log(isuserExist,is_user_exist,IsUserExist)
+
+// ARRAY []
+
+let students = ['laiba,maham,kainat,uzma']
+let student =[4]
+console.log(students)
